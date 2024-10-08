dae05dc0dcc8ae802dc83df8d76fe3088e271116
sagarraipurkar <er.sagarraipurkar@gmail.com>
2024-10-08 14:32:54 +0530

    Change in the email

--- src/test/java/SagarAcademy/errorMessage.java
+++ src/test/java/SagarAcademy/errorMessage.java
@@ -5,13 +5,12 @@ import org.testng.Assert;
 import basecomponent.BaseTest;
 import login.pagefactory.Login;
 import login.pagefactory.ProductCatalogs;
-
 public class errorMessage extends BaseTest {
 
 	public static void main(String[] args) throws Exception {
 
 		Login Login = LaunchApplication();
-		ProductCatalogs ProductCatalogs = Login.LoginApplication("Sagar@mailinator.com", "Sagar@91");
+		ProductCatalogs ProductCatalogs = Login.LoginApplication("Sagar@mail.com", "Sagar@91");
 		Assert.assertEquals("Incorrect email or password.", Login.LoginErrorMessage());
 	}
 
